6809236db7c39eb6add5e72a82ee0734b5a89839
lmarinve <lmarinve@gmail.com>
2022-11-30 09:39:38 -0500

    netcat sleep wait for mongodb replica

--- docker/sdx_containers/amlight/amlight_kytos.sh
+++ docker/sdx_containers/amlight/amlight_kytos.sh
@@ -17,4 +17,18 @@ if [ $COUNT -eq 0 ]; then
   echo "$HOSTS_ENTRY" >> /etc/hosts
 fi
 
-exec kytosd -l 192.168.0.2 -f
+sleep 60
+
+while ! nc -z 192.168.0.6 27027; do
+  sleep 1 # wait 1 second before check for mongo1t again
+done
+
+while ! nc -z 192.168.0.7 27028; do
+  sleep 1 # wait 1 second before check for mongo2t again
+done
+
+while ! nc -z 192.168.0.8 27029; do
+  sleep 1 # wait 1 second before check for mongo3t again
+done
+
+exec kytosd -l 192.168.0.2 -f --database mongodb
