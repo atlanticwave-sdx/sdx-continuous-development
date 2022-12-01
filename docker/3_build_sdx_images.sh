53b5ca0dbeb1c526f22c4317b3ecf0286287da25
lmarinve <lmarinve@gmail.com>
2022-12-01 16:07:48 -0500

    fixing mongodb dockerfile pointers and scripts

--- docker/3_build_sdx_images.sh
+++ docker/3_build_sdx_images.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker build -f ./sdx_containers/amlight/Dockerfile -t amlight .
-docker build -f ./sdx_containers/sax/Dockerfile -t sax .
-docker build -f ./sdx_containers/tenet/Dockerfile -t tenet .
+docker build -f ./sdx_containers/Dockerfile -t amlight .
+docker build -f ./sdx_containers/Dockerfile -t sax .
+docker build -f ./sdx_containers/Dockerfile -t tenet .
