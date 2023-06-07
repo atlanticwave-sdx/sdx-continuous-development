3fef9269335a114470ef5c671e5a5b10702f6f33
Ubuntu <ubuntu@ip-172-31-3-123.ec2.internal>
2023-06-07 21:02:14 +0000

    gunicon update

--- data-plane/scripts/amlight_sdx_lc_kytos.sh
+++ data-plane/scripts/amlight_sdx_lc_kytos.sh
@@ -15,13 +15,15 @@ while ! nc -z 192.168.0.8 27029; do
   sleep 1 # wait 1 second before check for mongo3t again
 done
 
-# tmux new-sess -d -s split-session ;\
-#	tmux send-keys -t split-session.0 \
-#	"kytosd -f --database mongodb" ENTER \
-#	tmux send-keys -t split-session.1 \
-#	"gunicorn 'swagger_server.__main__:main' -w 4 -b 0.0.0.0:8080" ENTER
-
 tmux new-sess -d -s k1 kytosd -f --database mongodb
+touch /var/log/gunicorn/access_gunicorn.log
+touch /var/log/gunicorn/error_gunicorn.log
+gunicorn 'swagger_server.__main__:main' -w 4 -b 0.0.0.0:8080 --log-level=debug \
+	--daemon \
+	--access-logfile var/log/gunicorn/access_gunicorn.log \
+	--error-logfile var/log/gunicorn/error_gunicorn.log \
+	--capture-output \
+	--log-level='debug'
 touch /var/log/kytos.log
 tail -f /var/log/kytos.log
 
--- data-plane/scripts/docker-ps.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-multipass exec sdx -- bash -c "docker ps"
