2cf87ee97716329cc1d7cbbce43cf367f1375585
Ubuntu <ubuntu@ip-172-31-3-123.ec2.internal>
2023-06-27 16:43:04 +0000

    sdx lc pipenv

--- data-plane/2_build_oxpos.sh
+++ data-plane/2_build_oxpos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker build --no-cache -f container-kytos-sdx-lc/kytos-sdx-topology/Dockerfile -t amlight .
-docker build --no-cache -f container-kytos-sdx-lc/kytos-sdx-topology/Dockerfile -t sax .
-docker build --no-cache -f container-kytos-sdx-lc/kytos-sdx-topology/Dockerfile -t tenet .
+docker build --no-cache -f container-kytos-sdx-topology/Dockerfile -t amlight .
+docker build --no-cache -f container-kytos-sdx-topology/Dockerfile -t sax .
+docker build --no-cache -f container-kytos-sdx-topology/Dockerfile -t tenet .
 docker build --no-cache -f container-sdx-controller/worker/Dockerfile -t worker .
