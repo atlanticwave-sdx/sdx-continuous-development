63279a2e4fd06adf20d5724accaebaf5e56e76ba
lmarinve <lmarinve@gmail.com>
2023-07-28 23:41:47 +0000

    updating submodules

--- data-plane/scripts/test_sdx_lc_kytos.sh
+++ data-plane/scripts/test_sdx_lc_kytos.sh
@@ -16,6 +16,15 @@ while ! nc -z 192.168.0.8 27029; do
 done
 
 tmux new-sess -d -s k1 kytosd -f --database mongodb
+touch /var/log/gunicorn/access_gunicorn.log
+touch /var/log/gunicorn/error_gunicorn.log
+pipenv install --dev
+pipenv run gunicorn 'swagger_server.__main__:app' -w 4 -b 0.0.0.0:8080 \
+	--daemon \
+	--access-logfile var/log/gunicorn/access_gunicorn.log \
+	--error-logfile var/log/gunicorn/error_gunicorn.log \
+	--capture-output \
+	--log-level='debug'
 touch /var/log/kytos.log
 tail -f /var/log/kytos.log
 
