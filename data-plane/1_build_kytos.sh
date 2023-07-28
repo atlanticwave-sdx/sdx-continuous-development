a5ae4c528f370e7fb676c5701fd741b2562d37b1
Italo Valcy S Brito <italovalcy@gmail.com>
2023-07-28 14:20:04 -0300

    Update 1_build_kytos.sh

--- data-plane/1_build_kytos.sh
+++ data-plane/1_build_kytos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker build -f os_base/kytos_base/Dockerfile -t kytos-base .
+docker build -f os_base/kytos_base/Dockerfile -t kytos_base .
 docker build --no-cache -f container-mininet/Dockerfile -t mininet .
 docker build --no-cache -f os_base/mongo_base/Dockerfile -t sdx-mongo .
 docker build --no-cache -f os_base/python_base/Dockerfile -t python-base .
