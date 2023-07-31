8b562753de4a7bc1cbfe16771638b7b7e956915e
lmarinve <lmarinve@gmail.com>
2023-07-31 03:23:44 +0000

    working on sdx_topology

--- data-plane/2_build_oxpos.sh
+++ data-plane/2_build_oxpos.sh
@@ -3,4 +3,3 @@ docker build --no-cache -f container-kytos-sdx-topology/Dockerfile -t amlight .
 docker build --no-cache -f container-kytos-sdx-topology/Dockerfile -t sax .
 docker build --no-cache -f container-kytos-sdx-topology/Dockerfile -t tenet .
 docker build --no-cache -f container-kytos-sdx-topology/Dockerfile -t sdx-test .
-docker build --no-cache -f container-worker/Dockerfile -t worker .
