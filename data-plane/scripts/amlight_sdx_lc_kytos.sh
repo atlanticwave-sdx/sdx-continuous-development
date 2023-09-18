e5acba013a9729d629e83ba2cdabecba3d68c0f3
lmarinve <lmarinve@gmail.com>
2023-09-18 13:06:03 +0000

    recreating the submodules pointers to main repositories #90

--- data-plane/scripts/amlight_sdx_lc_kytos.sh
+++ data-plane/scripts/amlight_sdx_lc_kytos.sh
@@ -4,15 +4,15 @@ set -eu
 sleep 30
 
 while ! nc -z 192.168.0.6 27027; do   
-  sleep 1 # wait 1 second before check for mongo1t again
+  sleep 5 # wait 1 second before check for mongo1t again
 done
 
 while ! nc -z 192.168.0.7 27028; do   
-  sleep 1 # wait 1 second before check for mongo2t again
+  sleep 5 # wait 1 second before check for mongo2t again
 done
 
 while ! nc -z 192.168.0.8 27029; do   
-  sleep 1 # wait 1 second before check for mongo3t again
+  sleep 5 # wait 1 second before check for mongo3t again
 done
 
 tmux new-sess -d -s k1 kytosd -f --database mongodb
