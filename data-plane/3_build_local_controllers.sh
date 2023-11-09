92180f8c574c44ddbf2a398c17cdbe2d43348ca7
lmarinve <lmarinve@gmail.com>
2023-11-09 10:32:02 -0500

    use the .env file instead of the settings.py file #93

--- data-plane/3_build_local_controllers.sh
+++ data-plane/3_build_local_controllers.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-docker build --no-cache -f container-sdx-lc/Dockerfile -t amlight-sdx-lc .
-docker build --no-cache -f container-sdx-lc/Dockerfile -t sax-sdx-lc .
-docker build --no-cache -f container-sdx-lc/Dockerfile -t tenet-sdx-lc .
-docker build --no-cache -f container-validator/Dockerfile -t validator .
+docker build --no-cache -f Dockerfile -t amlight-sdx-lc .
+docker build --no-cache -f Dockerfile -t sax-sdx-lc .
+docker build --no-cache -f Dockerfile -t tenet-sdx-lc .
