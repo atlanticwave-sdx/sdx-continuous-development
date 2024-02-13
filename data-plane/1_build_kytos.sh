78e19f57df8caed258681d9c16b78a2b9f6ad193
lmarinve <lmarinve@gmail.com>
2024-02-12 20:39:12 -0500

    kytos version amlight/kytos:2023.1 #100

--- data-plane/1_build_kytos.sh
+++ data-plane/1_build_kytos.sh
@@ -5,4 +5,5 @@ docker build --no-cache -f os_base/mongo_base/Dockerfile -t sdx-mongo .
 docker build --no-cache -f os_base/python_base/Dockerfile -t python-base .
 docker build --no-cache -f os_base/flask_base/Dockerfile -t flask-base .
 docker build --no-cache -f os_base/rabbit_base/Dockerfile -t sdx-rabbit .
+docker build --no-cache -f os_base/controller_base/Dockerfile -t controller-base .
 docker build --no-cache -f container-validator/Dockerfile -t validator .
