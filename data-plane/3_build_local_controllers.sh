9dd4b835745d3b09a05fce026891ad99ce0c0863
Italo Valcy <italo@ampath.net>
2024-06-05 11:10:08 -0300

    renaming scripts folder

--- data-plane/3_build_local_controllers.sh
+++ data-plane/3_build_local_controllers.sh
@@ -2,4 +2,5 @@
 docker build --no-cache -f ./Docker-files/sdx-lc/Dockerfile -t amlight-sdx-lc .
 docker build --no-cache -f ./Docker-files/sdx-lc/Dockerfile -t sax-sdx-lc .
 docker build --no-cache -f ./Docker-files/sdx-lc/Dockerfile -t tenet-sdx-lc .
-docker build --no-cache -f ./Docker-files/sdx-controller/Dockerfile -t sdx-controller .
+#docker build --no-cache -f ./Docker-files/sdx-controller/Dockerfile -t sdx-controller .
+cd container-sdx-controller; docker build -t sdx-controller .; cd -
