920d375a823bcac3b4e3ef3c65b4e61efa1463c7
Italo Valcy <italo@ampath.net>
2024-06-05 22:46:24 -0300

    Revert "renaming scripts folder"
    This reverts commit 9dd4b835745d3b09a05fce026891ad99ce0c0863.

--- data-plane/3_build_local_controllers.sh
+++ data-plane/3_build_local_controllers.sh
@@ -2,5 +2,4 @@
 docker build --no-cache -f ./Docker-files/sdx-lc/Dockerfile -t amlight-sdx-lc .
 docker build --no-cache -f ./Docker-files/sdx-lc/Dockerfile -t sax-sdx-lc .
 docker build --no-cache -f ./Docker-files/sdx-lc/Dockerfile -t tenet-sdx-lc .
-#docker build --no-cache -f ./Docker-files/sdx-controller/Dockerfile -t sdx-controller .
-cd container-sdx-controller; docker build -t sdx-controller .; cd -
+docker build --no-cache -f ./Docker-files/sdx-controller/Dockerfile -t sdx-controller .
