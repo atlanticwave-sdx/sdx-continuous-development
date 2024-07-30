74b76d5519ad037aeafac3a2687733e4d45dce1d
Italo Valcy <italo@ampath.net>
2024-07-30 16:04:09 -0300

    replace nc with mongosh and adding healthchecks to mongo and rabbit

--- data-plane/scripts/rs-add.sh
+++ data-plane/scripts/rs-add.sh
@@ -2,15 +2,15 @@
 
 sleep 30
 
-while ! nc -z 192.168.0.6 27027; do
+while ! echo 'db.runCommand("ping").ok' | mongosh --quiet "mongodb://192.168.0.6:27027/"; do
   sleep 1 # wait 1 second before check for mongo1t again
 done
 
-while ! nc -z 192.168.0.7 27028; do
+while ! echo 'db.runCommand("ping").ok' | mongosh --quiet "mongodb://192.168.0.7:27028/"; do
   sleep 1 # wait 1 second before check for mongo2t again
 done
 
-while ! nc -z 192.168.0.8 27029; do
+while ! echo 'db.runCommand("ping").ok' | mongosh --quiet "mongodb://192.168.0.8:27029/"; do
   sleep 1 # wait 1 second before check for mongo3t again
 done
 
@@ -110,4 +110,5 @@ sdx_controller.createUser(
 
 exit
 EOF
-
+# XXX: give enough time to healthcheck
+sleep 60
